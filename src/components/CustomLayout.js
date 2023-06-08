07c3ccc5993ad50e658339cd0e78e398b6d7979c
“nico-shishkin” <nicoshishkinatlogz@outlook.com>
2023-06-08 14:27:47 +0200

    -

--- src/components/CustomLayout.js
+++ src/components/CustomLayout.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import Tags from './Tags';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import Tags from '@site/src/components/Tags';
 import styles from './CustomLayout.module.css';
 
-export default function CustomLayout({ logo, tags, overviewTitle, children }) {
-  const logoUrl = useBaseUrl(logo);
-  
+const CustomLayout = ({ logo, tags, overviewTitle, children, containerTitle, header, items }) => {
   return (
     <div className={styles.customLayout}>
       <div className={styles.logoTags}>
-        <img src={logoUrl} alt="logo" className={styles.logo} />
+        <img src={logo} alt="Logo" className={styles.logo} />
         <Tags tags={tags} />
       </div>
-
-      <div>
-        <h2 className={styles.overview}>{overviewTitle}</h2>
+      <div className={styles.overview}>
+        <h1>{overviewTitle}</h1>
         <p className={styles.overviewText}>{children}</p>
+      </div>
+      <div className={styles.container}>
+        <h2>{containerTitle}</h2>
+        <h3>{header}</h3>
+        {items.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default CustomLayout;
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: (
-      <a href="/docs/intro">
-        AWS cloud services
-      </a>
-    ),
+    title: 'User Guide',
+    link: "/docs/user-guide/logzio/home-db",
     Svg: require('@site/static/img/aws.svg').default,
     description: (
       <>
-        Send logs, metrics and traces from your AWS cloud services to Logz.io.
+        Explore the comprehensive Logz.io User Guide and learn how to maximize your observability and security data.
       </>
     ),
   },
   {
-    title: 'Azure cloud services',
+    title: 'Integrations',
+    link: "/docs/shipping/sample",
     Svg: require('@site/static/img/azure.svg').default,
     description: (
       <>
@@ -25,20 +24,11 @@ const FeatureList = [
       </>
     ),
   },
-  {
-    title: 'Google cloud services',
-    Svg: require('@site/static/img/gcp.svg').default,
-    description: (
-      <>
-        Send logs, metrics and traces from your Google cloud services to Logz.io.
-      </>
-    ),
-  },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, title, description, link}) {
   return (
-    <div className={clsx('col col--4')}>
+    <Link to={link} className={clsx('col col--4', styles.featureCard)}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -46,15 +36,15 @@ function Feature({Svg, title, description}) {
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+      <div className={clsx('container', styles.featuresContainer)}>
+        <div className={clsx('row', styles.centeredRow)}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
@@ -63,3 +53,4 @@ export default function HomepageFeatures() {
     </section>
   );
 }
+
