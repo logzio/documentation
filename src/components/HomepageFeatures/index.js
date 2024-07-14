8cb8816b9f75fb4b750671342a21c0e9d8b4f89c
“nico-shishkin” <nicoshishkinatlogz@outlook.com>
2024-07-14 12:25:49 +0100

    Updates

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,85 @@ import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 import SearchHeader from '../../components/SearchHeader';  // Already imported
 
-const FeatureList = [
+const InfrastructureEnvironmentFeatures = [
+  {
+    link: "/docs/user-guide/quick-start",
+    Svg: require('@site/static/img/quick-start.svg').default,
+  },
+  {
+    link: "/docs/user-guide/observability/assistantiq",
+    Svg: require('@site/static/img/obsiq.svg').default,
+  },
+  {
+    link: "/docs/user-guide/new-explore/",
+    Svg: require('@site/static/img/explore.svg').default,
+  },
+  {
+    link: "/docs/user-guide/k8s-360/overview",
+    Svg: require('@site/static/img/k8s.svg').default,
+  },
+  {
+    link: "/docs/category/send-your-data",
+    Svg: require('@site/static/img/integrations.svg').default,
+  },
+  {
+    link: "https://api-docs.logz.io/docs/category/logz-api",
+    Svg: require('@site/static/img/api.svg').default,
+  },
+];
+
+const ApplicationMonitoringFeatures = [
+  {
+    link: "/docs/user-guide/telemetry-collector/",
+    Svg: require('@site/static/img/telemetry-collector.svg').default,
+  },
+  {
+    link: "/docs/user-guide/log-management/troubleshooting/log-shipping-troubleshooting",
+    Svg: require('@site/static/img/troubleshooting.svg').default,
+  },
+  {
+    link: "/docs/user-guide/admin/logzio-accounts/accounts",
+    Svg: require('@site/static/img/accounts.svg').default,
+  },
+  {
+    link: "https://logz.io/freetrial/",
+  },
+  {
+    link: "https://logz.io/learn/webinar/",
+  },
+  {
+    link: "https://logz.io/learn/product/",
+  },
+];
+
+const PopularIntegrationsFeatures = [
+  {
+    link: "/docs/user-guide/quick-start",
+    Svg: require('@site/static/img/quick-start.svg').default,
+  },
+  {
+    link: "/docs/user-guide/observability/assistantiq",
+    Svg: require('@site/static/img/obsiq.svg').default,
+  },
+  {
+    link: "/docs/user-guide/new-explore/",
+    Svg: require('@site/static/img/explore.svg').default,
+  },
+  {
+    link: "/docs/user-guide/k8s-360/overview",
+    Svg: require('@site/static/img/k8s.svg').default,
+  },
+  {
+    link: "/docs/category/send-your-data",
+    Svg: require('@site/static/img/integrations.svg').default,
+  },
+  {
+    link: "https://api-docs.logz.io/docs/category/logz-api",
+    Svg: require('@site/static/img/api.svg').default,
+  },
+];
+
+const ObservabilitySuiteFeatures = [
   {
     title: 'Logz.io quick start guide',
     link: "/docs/user-guide/quick-start",
@@ -41,48 +119,24 @@ const FeatureList = [
     Svg: require('@site/static/img/api.svg').default,
     description: <>Grow your own integration.</>,
   },
-  {
-    title: 'Telemetry Collector',
-    link: "/docs/user-guide/telemetry-collector/",
-    Svg: require('@site/static/img/telemetry-collector.svg').default,
-    description: <>Send logs, metrics, and traces data quickly and easily.</>,
-  },
-  {
-    title: 'Log troubleshooting',
-    link: "/docs/user-guide/log-management/troubleshooting/log-shipping-troubleshooting",
-    Svg: require('@site/static/img/troubleshooting.svg').default,
-    description: <>Troubleshoot common log related issues.</>,
-  },
-  {
-    title: 'Your accounts',
-    link: "/docs/user-guide/admin/logzio-accounts/accounts",
-    Svg: require('@site/static/img/accounts.svg').default,
-    description: <>Manage your accounts & optimize costs.</>,
-  },
-  {
-    title: 'Free trial',
-    link: "https://logz.io/freetrial/",
-    description: <></>,
-  },
-  {
-    title: 'Webinars',
-    link: "https://logz.io/learn/webinar/",
-    description: <></>,
-  },
-  {
-    title: 'Features',
-    link: "https://logz.io/learn/product/",
-    description: <></>,
-  },
 ];
 
-function Feature({Svg, title, description, link}, index) {
+function Feature({Svg, link}) {
   return (
-    <Link to={link} className={clsx('col col--3', styles.featureCard, index >= 9 ? styles.smallCard : '')}>
+    <Link to={link} className={clsx('col col--4', styles.featureCard)}>
       <div className="text--center">
         {Svg && <Svg className={styles.featureSvg} role="img" />}
       </div>
+    </Link>
+  );
+}
 
+function FeatureWithDetails({Svg, link, title, description}) {
+  return (
+    <Link to={link} className={clsx('col col--4', styles.featureCardWithDetails)}>
+      <div className="text--center">
+        {Svg && <Svg className={styles.featureSvg} role="img" />}
+      </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
@@ -91,15 +145,95 @@ function Feature({Svg, title, description, link}, index) {
   );
 }
 
+function StaticCard({ title, description }) {
+  return (
+    <div className={clsx('col col--12', styles.staticCard)}>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function FeatureSection({header, features, showLink, isDetailed}) {
+  return (
+    <div className="col col--4">
+      <h3 className="text--center">{header}</h3>
+      <div className="row">
+        {features.map((props, idx) => (
+          isDetailed ? 
+          <FeatureWithDetails key={idx} {...props} /> :
+          <Feature key={idx} {...props} />
+        ))}
+      </div>
+      {showLink && (
+        <div className="text--center" style={{ marginTop: '20px' }}>
+          <Link to="/docs/user-guide/quick-start" className={styles.seeAllLink}>
+            See All Integrations
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function PopularLinks() {
+  return (
+    <div className={styles.popularLinks}>
+      Popular: 
+      <Link to="/docs/user-guide/quick-start">Agent quick setup</Link>
+      <span className={styles.separator}>|</span>
+      <Link to="/docs/user-guide/k8s-360/overview">K8S</Link>
+      <span className={styles.separator}>|</span>
+      <Link to="/docs/user-guide/new-explore/">Metrics</Link>
+      <span className={styles.separator}>|</span>
+      <Link to="/docs/category/send-your-data">Integration setup</Link>
+      <span className={styles.separator}>|</span>
+      <Link to="https://api-docs.logz.io/docs/category/logz-api">API</Link>
+    </div>
+  );
+}
+
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className={clsx('container', styles.featuresContainer)}>
-        <SearchHeader /> {/* Added SearchHeader component here */}
-        <div className={clsx('row', styles.centeredRow)}>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+        <div className="row">
+          <div className="col col--12 text--center" style={{ marginBottom: '20px' }}>
+            <h1 className={styles.mainTitle}>Logz.io Documentation</h1>
+          </div>
+          <div className="col col--12 text--center" style={{ marginBottom: '20px' }}>
+            <div className={clsx(styles.searchContainer)}>
+              <SearchHeader /> {/* Centered and wider SearchHeader component here */}
+            </div>
+          </div>
+          <div className="col col--12 text--center" style={{ marginBottom: '20px' }}>
+            <PopularLinks /> {/* Popular links under the search bar */}
+          </div>
+          <div className="col col--12 text--center" style={{ marginBottom: '20px' }}>
+            <h1 className={styles.stepTitle}>Getting Started with Logz.io</h1> {/* Centered heading here */}
+          </div>
+          <div className="col col--12 text--center" style={{ marginBottom: '20px' }}>
+            <h2 className={styles.stepSubtitle}>Step 1: Ship your data</h2> {/* Centered heading here */}
+          </div>
+        </div>
+        <div className="row">
+          <FeatureSection header="Infrastructure Environment" features={InfrastructureEnvironmentFeatures} showLink={false} isDetailed={false} />
+          <FeatureSection header="Application Monitoring" features={ApplicationMonitoringFeatures} showLink={true} isDetailed={false} />
+          <FeatureSection header="Popular Integrations" features={PopularIntegrationsFeatures} showLink={false} isDetailed={false} />
+        </div>
+        <div className="row" style={{ marginTop: '40px' }}>
+          <div className="col col--12 text--center" style={{ marginBottom: '20px' }}>
+            <h2 className={styles.stepSubtitle}>Step 2: Get the 360 Observability Suite</h2> {/* Centered heading for Step 2 */}
+          </div>
+          <div className="col col--12">
+            <div className="row">
+              {ObservabilitySuiteFeatures.map((props, idx) => (
+                <FeatureWithDetails key={idx} {...props} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
