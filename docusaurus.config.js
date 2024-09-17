12b9cd6c64d8f26faf2019fbedab3a628825290e
Simplychee <simplychee@gmail.com>
2024-09-17 14:40:38 +0300

    contextualSearch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
         indexName: 'shiny-alfajores-ede8d8',
   
         // Optional: see doc section below
-        contextualSearch: true,
+        contextualSearch: false,
   
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: 'external\\.com|domain\\.com',
